f679e4b1f8708aaafeda3681d7864b7e4c736289
alexnaev <102225427+alexnaev@users.noreply.github.com>
2022-04-21 16:46:06 +0200

    undo

--- Huskjen/Program.cs
+++ Huskjen/Program.cs
@@ -19,12 +19,12 @@ var dbContext = app.Services
 await dbContext.Database.MigrateAsync();
 
 // Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
